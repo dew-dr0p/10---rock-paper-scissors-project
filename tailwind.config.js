dc5f4db8b56b4796a49493c37e957e2c3dfbc805
Wisdom-Daniel <wisdom-daniel@Wisdom-Daniels-MacBook-Air.local>
2023-07-03 14:01:00 +0100

    Initialised basic requirements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
+    colors: {
+      gradients: {
+        backgroundA: '#1F3756',
+        backgroundB: '#141539',
+        scissorsA: '#EC9E0E',
+        scissorsB: '#ECA922',
+        paperA: '#4865F4',
+        paperB: '#5671F5',
+        lizardA: '#834FE3',
+        lizardB: '#8C5DE5',
+        rockA: '#DC2E4E',
+        rockB: '#DD405D',
+        cyanA: '#40B9CE',
+        cyanB: '#52BED1'
+      },
+      text: {
+        dark: '#3B4363',
+        score: '#2A46C0',
+        headerOutline: '#606E85'
+      },
+    },
     extend: {},
   },
   plugins: [],
