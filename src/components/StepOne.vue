cd7e17d8991fd359f2481919d2b4c5f043236d3c
Wisdom-Daniel Efe <efewisdomd01@gmail.com>
2023-07-05 17:30:41 +0100

    Added pointer-cursor for elements to show them clickable

--- src/components/StepOne.vue
+++ src/components/StepOne.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="absolute justify-self-center self-center">
             <div class="bg-[url('../assets/images/bg-triangle.svg')] bg-no-repeat relative w-[20rem] h-[17.5rem]">
-                <div @click="getUserChoice" id="paper" class="bg-[#FFF] rounded-full w-40 h-40 justify-center items-center flex border-[20px] border-gradients-paperA absolute -top-20 -left-20">
+                <div @click="getUserChoice" id="paper" class="bg-[#FFF] rounded-full w-40 h-40 justify-center items-center flex border-[20px] border-gradients-paperA absolute -top-20 -left-20 cursor-pointer">
                     <img id="paper" src="../assets/images/icon-paper.svg" alt="">
                 </div>
-                <div @click="getUserChoice" id="scissors" class="bg-[#FFF] rounded-full w-40 h-40 justify-center items-center flex border-[20px] border-gradients-scissorsA absolute -top-20 -right-20">
+                <div @click="getUserChoice" id="scissors" class="bg-[#FFF] rounded-full w-40 h-40 justify-center items-center flex border-[20px] border-gradients-scissorsA absolute -top-20 -right-20 cursor-pointer">
                     <img id="scissors" src="../assets/images/icon-scissors.svg" alt="">
                 </div>
-                <div @click="getUserChoice" id="rock" class="bg-[#FFF] rounded-full w-40 h-40 justify-center items-center flex border-[20px] border-gradients-rockA absolute -bottom-20 left-20">
+                <div @click="getUserChoice" id="rock" class="bg-[#FFF] rounded-full w-40 h-40 justify-center items-center flex border-[20px] border-gradients-rockA absolute -bottom-20 left-20 cursor-pointer">
                     <img id="rock" src="../assets/images/icon-rock.svg" alt="">
                 </div>
             </div>
