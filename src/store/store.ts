150d2e48f47ca207e283ef8c322d46b3262febfb
Wisdom-Daniel <44666155+dew-dr0p@users.noreply.github.com>
2023-07-07 18:26:59 +0100

    seperated bonusStore from store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,26 +2,16 @@ import { ref } from 'vue'
 import StepOne from '../components/StepOne.vue'
 import StepTwo from '../components/StepTwo.vue'
 import StepThree from '../components/StepThree.vue'
-import StepFour from '../components/StepFour.vue'
-import BonusStepOne from '@/components/BonusStepOne.vue'
 
 const components: any = {
     StepOne,
     StepTwo,
     StepThree,
-    StepFour
 }
 
 const component = ref('StepOne')
 
-const bonusComponents: any = {
-    BonusStepOne,
-}
-
-const bonusComponent = ref('BonusStepOne')
-
 const choices = ["rock", "paper", "scissors"]
-// const bonusChoices = ["rock", "spock", "paper", "lizard", "scissors"]
 
 const map: Record<string, any> = {}
 // let map = new Map()
@@ -49,21 +39,6 @@ choices.forEach((choice, i) => {
     // map[choice][choices[(i+2)%3]] = choice + " wins"
 })
 
-// bonusChoices.forEach((choice, i) => {
-//     console.log(map)
-//     map[choice] = {};
-//     for (let j = 0, half = (bonusChoices.length-1)/2; j < bonusChoices.length; j++) {
-//         const opposition = (i+j)%bonusChoices.length
-//         if (!j)
-//             map[choice][choice] = "Tie"
-//         else if (j <= half)
-//             map[choice][bonusChoices[opposition]] = bonusChoices[opposition] + " wins"
-//         else
-//             map[choice][bonusChoices[opposition]] = choice + " wins"
-//     }
-//     console.log(map)
-// })
-
 const score = ref(0)
 
 const computerChoice = ref('')
@@ -73,13 +48,13 @@ const userElement = ref()
 const computerElement = ref()
 
 const rockElement = `<div id="rock" class="bg-[#FFF] rounded-full w-40 h-40 justify-center items-center flex border-[20px] border-gradients-rockA scale-150">
-<img id="rock" src="../assets/images/icon-rock.svg" alt="">
+<img id="rock" src="src/assets/images/icon-rock.svg" alt="">
 </div>`
 const paperElement = `<div id="paper" class="bg-[#FFF] rounded-full w-40 h-40 justify-center items-center flex border-[20px] border-gradients-paperA scale-150">
-<img id="paper" src="../assets/images/icon-paper.svg" alt="">
+<img id="paper" src="src/assets/images/icon-paper.svg" alt="">
 </div>`
 const scissorsElement = `<div id="scissors" class="bg-[#FFF] rounded-full w-40 h-40 justify-center items-center flex border-[20px] border-gradients-scissorsA scale-150">
-<img id="scissors" src="../assets/images/icon-scissors.svg" alt="">
+<img id="scissors" src="src/assets/images/icon-scissors.svg" alt="">
 </div>`
 
 
@@ -109,23 +84,8 @@ const generateComputerChoice = () => {
     }
 }
 
-const generateBonusComputerChoice = () => {
-    if (Math.random() < 0.2) {
-        computerChoice.value = 'rock'
-    } else if (Math.random() < 0.4) {
-        computerChoice.value = 'spock'
-    } else if (Math.random() < 0.6) {
-        computerChoice.value = 'paper'
-    } else if (Math.random() < 0.8) {
-        computerChoice.value = 'lizard'
-    } else {
-        computerChoice.value = 'scissors'
-    }
-}
-
 // Using Record or Object
 const getResult = (choice1: string, choice2: string) => (map[choice1] || {})[choice2] || "Invalid choice"
-// const getBonusResult = (choice1: string, choice2: string) => console.log((map[choice1] || {})[choice2] || "Invalid choice")
 
 const updateScore = (value: string) => {
     value === 'increase' ? score.value++ : (value === 'decrease' ? score.value-- : console.log("Invalid Score"))
@@ -145,15 +105,12 @@ function resetAll() {
 export {
     components,
     component,
-    bonusComponents,
-    bonusComponent,
     score,
     userElement,
     userChoice,
     computerElement,
     computerChoice,
     generateComputerChoice,
-    generateBonusComputerChoice,
     getResult,
     getUserChoice,
     updateScore,
