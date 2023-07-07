f2f5e9b96963b1f1eba17fd5a169dfb41fbdea9c
Wisdom-Daniel <44666155+dew-dr0p@users.noreply.github.com>
2023-07-07 18:26:34 +0100

    adjusted gap between logo and score

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="p-7 border-2 border-[#FFF] rounded-2xl max-w-[50vw] m-auto border-opacity-50 uppercase">
-        <div class="grid grid-cols-2 gap-80">
+        <div class="grid grid-cols-2 gap-40">
             <img src="../assets/images/logo.svg" alt="Rock Paper Scissor Logo">
             <div class="bg-[#FFF] text-text-dark justify-center items-center flex flex-col rounded-xl">
                 <p class="text-gradients-paperA text-xl">Score</p>
